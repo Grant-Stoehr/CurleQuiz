9990d7146abdd96139c33c20b8caa6ffe1cfce38
grant-stoehr <gstoehr@smu.edu>
2023-02-23 10:17:36 -0600

    First run of app.js:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+let nextId = 0;
+
+const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [ingredients, addIngredients] = useState([]);
+
+  const questions = [
+    {
+      question: 'What is the shape of your hair strand?',
+      answers: [
+        { option: 'Straight', ingredient: "feather" },
+        { option: 'Wavy', ingredient: "feather" },
+        { option: 'Curly', ingredient: "feather" },
+        { option: 'Oily', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'How would you describe the diameter of your hair strand?',
+      answers: [
+        { option: 'Fine', ingredient: "feather" },
+        { option: 'Medium', ingredient: "feather" },
+        { option: 'Coarse', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'When you stretch a strand of your hair, how much does it stretch before breaking?',
+      answers: [
+        { option: 'A lot', ingredient: "feather" },
+        { option: 'A little', ingredient: "feather" },
+        { option: 'Not at all', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'How well does your hair absorb and retain moisture?',
+      answers: [
+        { option: 'Easily and quickly', ingredient: "feather" },
+        { option: 'Slowly and with difficulty', ingredient: "feather" },
+        { option: 'Not at all', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'How much hair do you have on your scalp?',
+      answers: [
+        { option: 'Thin', ingredient: "feather" },
+        { option: 'Medium', ingredient: "feather" },
+        { option: 'Thick', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'How often do you experience flaking, itching or soreness on your scalp?',
+      answers: [
+        { option: 'Never', ingredient: "feather" },
+        { option: 'Sometimes', ingredient: "feather" },
+        { option: 'Often', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'What type of climate or weather do you live in?',
+      answers: [
+        { option: 'Hot and humid', ingredient: "feather" },
+        { option: 'Cold and dry', ingredient: "feather" },
+        { option: 'Mild and temperate', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'How often do you style your hair with heat or chemical treatments?',
+      answers: [
+        { option: 'Never', ingredient: "feather" },
+        { option: 'Occasionally', ingredient: "feather" },
+        { option: 'Frequently', ingredient: "feather" },
+      ],
+    },
+    {
+      question: 'Are you allergic to any hair care ingredients?',
+      answers: [
+        { option: 'Yes', ingredient: "feather" },
+        { option: 'No', ingredient: "feather" },
+      ],
+    },
+  ];
+
+  const handleAnswer = (ingredient) => {
+    ingredients.push({
+      id: nextId++,
+      ingredient: ingredient,
+    });
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      // Create a way to display all the ingredients that will be used in the recipe
+      // and then reset the quiz
+      console.log(ingredients);
+      setCurrentQuestion(0);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='quiz'>
+      <h2>Quiz</h2>
+      <div className='question'>
+        {questions[currentQuestion].question}
+      </div>
+      <div className='answers'>
+        {questions[currentQuestion].answers.map((answer, index) => (
+          <button
+            key={index}
+            onClick={() => handleAnswer(answer.correct)}
+          >
+            {answer.option}
+          </button>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Quiz;
